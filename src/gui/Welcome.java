cec12f8a2d3b143a19fe135c24fbc73fd9f4e326
verenaseverina <verenaseverina@gmail.com>
2017-04-16 12:58:27 +0700

    Tambahin dikit

--- src/gui/Welcome.java
+++ src/gui/Welcome.java
@@ -3,7 +3,7 @@ package gui;
 import javax.swing.*;
 
 public class Welcome extends JPanel {
-  private static void introductionPanel() {
+  public Welcome() {
     JFrame frame = new JFrame();
     String text = "Welcome to Monius, Money Genius!";
     JOptionPane.showMessageDialog(frame, text, "Welcome", JOptionPane.PLAIN_MESSAGE);
