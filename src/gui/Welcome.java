11898bb3f2c81dc574623f70a9fdb1173e00ab56
verenaseverina <verenaseverina@gmail.com>
2017-04-16 13:01:27 +0700

    Biar muncul di layarnya

--- src/gui/Welcome.java
+++ src/gui/Welcome.java
@@ -7,5 +7,9 @@ public class Welcome extends JPanel {
     JFrame frame = new JFrame();
     String text = "Welcome to Monius, Money Genius!";
     JOptionPane.showMessageDialog(frame, text, "Welcome", JOptionPane.PLAIN_MESSAGE);
+    frame.setSize(200,100);
+    frame.setVisible(true);
+    frame.setLocationRelativeTo(null);
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
   }
 }
