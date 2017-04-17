7b09e66d6441e935105313d1301dedb35b5defe5
verenaseverina <verenaseverina@gmail.com>
2017-04-17 21:32:45 +0700

    javadocs newdate

--- src/financialrecords/records/NewDate.java
+++ src/financialrecords/records/NewDate.java
@@ -1,14 +1,31 @@
 package financialrecords.records;
 
 /**
+ * Kelas untuk tanggal.
+ * Kelas financialrecords.records.NewDate memiliki atribut String untuk hari,
+ * String untuk bulan, dan String untuk tahun
  *
- * @author nim_13515091
+ * @author Kukuh B R
+ * @author Adrian Hartanto
  */
 public class NewDate {
+  /**
+   * Atribut utnuk menyimpan hari dari tanggal.
+   */
   private String day;
+  /**
+   * Atribut utnuk menyimpan bulan dari tanggal.
+   */
   private String month;
+  /**
+   * Atribut utnuk menyimpan tahun dari tanggal.
+   */
   private String year;
-  
+
+  /**
+   * Ctor NewDate.
+   * @param tanggal adalah String yang akan dibentuk menjadi NewDate
+   */
   public NewDate(String tanggal) {
     String dd = tanggal.substring(0, 2);
     String mm = tanggal.substring(3, 5);
@@ -17,6 +34,11 @@ public class NewDate {
     month = mm;
     year = yy;
   }
+
+  /**
+   * Method copy NewDate.
+   * @param tanggal adalah tanggal yang hendak di-copy
+   */
   public void copyDate(String tanggal) {
     String dd = tanggal.substring(0, 2);
     String mm = tanggal.substring(3, 5);
@@ -25,7 +47,11 @@ public class NewDate {
     month = mm;
     year = yy;
   }
-  
+
+  /**
+   * Konversi dari NewDate ke String.
+   * @return hasil konversi dari NewDate ke String
+   */
   public String dateToStr() {
     String date = day;
     date = date.concat("/");
@@ -34,31 +60,62 @@ public class NewDate {
     date = date.concat(year);
     return date;
   }
-  
+
+  /**
+   * Getter day.
+   * @return day
+   */
   public String getDay() {
     return day;
   }
-  
+
+  /**
+   * Getter month.
+   * @return month
+   */
   public String getMonth() {
     return month;
   }
-  
+
+  /**
+   * Getter year.
+   * @return year
+   */
   public String getYear() {
     return year;
   }
-  
+
+  /**
+   * Setter day.
+   * @param dd String yang akan diassign ke day
+   */
   public void setDay(String dd) {
     day = dd;
   }
-  
+
+  /**
+   * Setter month.
+   * @param mm String yang akan diassign ke month
+   */
   public void setMonth(String mm) {
     month = mm;
   }
-  
+
+
+  /**
+   * Setter year.
+   * @param yy String yang akan diassign ke year
+   */
   public void setYear(String yy) {
     year = yy;
   }
-  
+
+  /**
+   * Mengecek apakah dua tanggal berada di minggu yang sama.
+   * @param date1 tanggal1 yang akan dibandingkan
+   * @param date2 pembanding tanggal1
+   * @return apakah dua tanggal berada di minggu yang sama
+   */
   public boolean isOneWeek(NewDate date1, NewDate date2) {
     int day1 = Integer.getInteger(date1.getMonth());
     int day2 = Integer.getInteger(date2.getMonth());
@@ -66,7 +123,13 @@ public class NewDate {
     boolean yearEquals = date1.getYear().equals(date2.getYear());
     return (monthEquals && yearEquals && (day1 + 7 == day2));
   }
-  
+
+  /**
+   * Mengecek apakah dua tanggal berada di bulan yang sama.
+   * @param date1 tanggal1 yang akan dibandingkan
+   * @param date2 pembanding tanggal1
+   * @return apakah dua tanggal berada di bulan yang sama
+   */
   public boolean isOneMonth(NewDate date1, NewDate date2) {
     int month1 = Integer.getInteger(date1.getMonth());
     int month2 = Integer.getInteger(date2.getMonth());
@@ -74,7 +137,13 @@ public class NewDate {
     boolean yearEquals = date1.getYear().equals(date2.getYear());
     return (dayEquals && yearEquals && (month1 + 1 == month2));
   }
-  
+
+  /**
+   * Mengecek apakah dua tanggal berada di tahun yang sama.
+   * @param date1 tanggal1 yang akan dibandingkan
+   * @param date2 pembanding tanggal1
+   * @return apakah dua tanggal berada di tahun yang sama
+   */
   public boolean isOneYear(NewDate date1, NewDate date2) {
     int year1 = Integer.getInteger(date1.getYear());
     int year2 = Integer.getInteger(date2.getYear());
