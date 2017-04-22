ac4d26917bb04e4221dbe3b3ea6ff641282eb243
verenaseverina <verenaseverina@gmail.com>
2017-04-22 18:46:46 +0700

    Baru background swingnya belom ada buttonnya

--- src/financialrecords/FinancialRecordView.java
+++ src/financialrecords/FinancialRecordView.java
@@ -1,5 +1,36 @@
 package financialrecords;
 
-public class FinancialRecordView {
+import java.awt.*;
+import javax.swing.*;
 
+public class FinancialRecordView extends JFrame {
+  // bisa tambahin ga dia lagi nampilin apaan
+  private int width = 750;
+  private int height = 500;
+
+  public FinancialRecordView() {
+    this.setVisible(true);
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    JPanel controls = new JPanel();
+    controls.setSize(200,500);
+    controls.setBackground(Color.white);
+    controls.setVisible(true);
+
+    JPanel glass = new JPanel();
+    glass.setSize(550,500);
+    glass.setBackground(Color.black);
+    glass.setVisible(true);
+
+    // biar resizable pake JSplitPane
+    JSplitPane splitPane = new JSplitPane();
+    splitPane.setSize(width,height);
+    splitPane.setDividerSize(0);
+    splitPane.setDividerLocation(200);
+    splitPane.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
+    splitPane.setLeftComponent(controls);
+    splitPane.setRightComponent(glass);
+
+    this.add(splitPane);
+  }
 }
