05c09d4f4433e6c71b4d7bcc75d8028ff2cc5acf
kukuhbr <kukuhis777@gmail.com>
2017-04-22 14:07:59 +0700

    test

---
+++ src/signin/SignInView.java
@@ -0,0 +1,7 @@
+package signin;
+
+/**
+ * Created by KUKUH BASUKI RAHMAT on 22-Apr-17.
+ */
+public class SignInView {
+}
--- test/parse/ParseTest.java
+++ test/parse/ParseTest.java
@@ -11,11 +11,9 @@ package parse;
  */
 
 import java.util.ArrayList;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+
+import org.junit.*;
+
 import static org.junit.Assert.*;
 
 /**
