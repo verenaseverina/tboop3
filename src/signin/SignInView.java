14aa5b76f990affd2f624964b46c641ede73660d
kukuhbr <kukuhis777@gmail.com>
2017-04-23 16:47:07 +0700

    Merge branch 'master' of C:\Users\KUKUH BASUKI RAHMAT\Documents\GitHub\tboop3 with conflicts.

--- src/signin/SignInView.java
+++ src/signin/SignInView.java
@@ -1,68 +1,20 @@
 package signin;
 
-
-import financialrecords.FinancialRecordView;
-
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
  * Created by KUKUH BASUKI RAHMAT on 22-Apr-17.
  */
-
 public class SignInView extends JFrame {
-  private int width = 200;
-  private int height = 150;
-  private SignInController signInP;
-  private SignInModel signInM;
+  private int width = 500;
+  private int height = 250;
 
   public SignInView() {
     this.setVisible(true);
+    this.setResizable(false);
     this.setLocationRelativeTo(null);
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-    JPanel panel = new JPanel();
-    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-    JLabel passwtxt = new JLabel("Password : ");
-    passwtxt.setAlignmentX(Component.CENTER_ALIGNMENT);
-    panel.add(passwtxt);
-
-    JTextField inputPassword = new JTextField();
-    inputPassword.setAlignmentX(Component.CENTER_ALIGNMENT);
-    panel.add(inputPassword);
-
-    String txt = "Hint : " + signInM.getDescription();
-    JLabel hint = new JLabel(txt);
-    hint.setAlignmentX(Component.CENTER_ALIGNMENT);
-    hint.setVisible(false);
-    panel.add(hint);
-
-
-    JButton check = new JButton("OK");
-    check.setBackground(Color.black);
-    check.setAlignmentX(Component.CENTER_ALIGNMENT);
-    panel.add(check);
-
-    check.addActionListener(
-            new ActionListener() {
-              @Override
-              public void actionPerformed(ActionEvent e) {
-                String inPass = new String(inputPassword.getText());
-                if (signInP.validatePassword(inPass)) {
-                  FinancialRecordView buf = new FinancialRecordView();
-                } else {
-                  showAlert();
-                  hint.setVisible(true);
-                }
-              }
-            }
-    );
-  }
-
-  public void showAlert() {
-    JFrame alertFr = new JFrame();
-    JOptionPane.showMessageDialog(alertFr, "Wrong Password", "Error", JOptionPane.ERROR_MESSAGE);
+    
   }
 }
