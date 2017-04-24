6bd9e6dbf9cf685b55fa5237a2af883c752e3e57
kukuhbr <kukuhis777@gmail.com>
2017-04-24 09:55:11 +0700

    main bisa welcome :)

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import gui.Welcome;
+import signin.MvcSignIn;
+
+/**
+ * Created by KUKUH BASUKI RAHMAT on 23-Apr-17.
+ */
+public class Main {
+  public static void main(String args[]) {
+    Welcome welc = new Welcome();
+    MvcSignIn signin = new MvcSignIn();
+  }
+}
