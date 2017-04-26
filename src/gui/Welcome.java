8be777349c15c2ba1befc35a052b71f858817210
verenaseverina <verenaseverina@gmail.com>
2017-04-26 09:11:41 +0700

    Javadocs Welcome

--- src/gui/Welcome.java
+++ src/gui/Welcome.java
@@ -2,7 +2,16 @@ package gui;
 
 import javax.swing.*;
 
+/**
+ * Kelas untuk menampilkan pesan welcome.
+ *
+ * @author Verena Severina
+ * @author Kukuh B R
+ */
 public class Welcome extends JPanel {
+  /**
+   * Constructor Welcome.
+   */
   public Welcome() {
     JFrame frame = new JFrame();
     String text = "Welcome to Monius, Money Genius!";
